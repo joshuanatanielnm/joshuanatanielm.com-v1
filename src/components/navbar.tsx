0ad01d0a810dc48805dfd07cad8ecb194296a025
Joshua Nathaniel M <clsjoshua@gmail.com>
2021-02-13 19:15:41 +0700

    first commit

---
+++ src/components/navbar.tsx
@@ -0,0 +1,15 @@
+import {} from "@chakra-ui/react";
+
+import * as React from "react";
+
+import NextLink from "next/link";
+
+const Navbar:React.FC = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Navbar;
