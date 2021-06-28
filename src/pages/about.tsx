dfaaa222e253e3a5528556b755cc1537d66fcffc
Joshua Nathaniel M <clsjoshua@gmail.com>
2021-06-29 01:09:38 +0700

    feat: update about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -5,7 +5,7 @@ import IlusProfile from '@/components/ilus-profile'
 import { NextSeo } from "next-seo";
 import React from 'react'
 
-const About:React.FC = () => {
+const About: React.FC = () => {
   return (
     <>
       <Head>
@@ -29,15 +29,15 @@ const About:React.FC = () => {
         }}
       />
 
-      <Box fontFamily='poppins' bgGradient="linear(to-br, white, blue.100)"  borderRadius="xl" p={{base:8, md:24}} spacing={6}>
+      <Box fontFamily='poppins' bgGradient="linear(to-br, white, blue.100)" borderRadius="xl" p={{ base: 8, md: 24 }} spacing={6}>
         <VStack textAlign='center' py='50px' spacing='24px'>
           <IlusProfile />
           <Heading fontFamily='poppins' as='h1' size='2xl'>
-            Hello i'am Jojo. 👋
+            Hello All i'am Jojo. 👋
           </Heading>
-          <Box fontSize={{base:'xl', md:'2xl'}} textAlign='left'>
+          <Box fontSize={{ base: 'xl', md: '2xl' }} textAlign='left'>
             <Text >
-              I’am a self taught Website developer based in <span style={{fontWeight:'bold'}}>Surabaya, Indonesia</span>, currently i working on my bachelor's degree in information system on <span style={{fontWeight:'bold'}}>Institut Teknologi Adhi Tama Surabaya</span> and doing some freelance projects.
+              I’am a self taught Website developer based in <span style={{ fontWeight: 'bold' }}>Surabaya, Indonesia</span>, currently i working on my bachelor's degree in information system on <span style={{ fontWeight: 'bold' }}>Institut Teknologi Adhi Tama Surabaya</span> and doing some freelance projects.
             </Text>
             <Text>
               i start learning about web dev since 2018, and start with backend development using php and laravel. but for now i spend more time to explore about frontend development espicially using Reactjs.
