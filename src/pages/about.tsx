40fd65ce8eabb523e139d3d35618b249ae11ae1e
Joshua Nathaniel M <clsjoshua@gmail.com>
2021-08-09 21:30:02 +0700

    feat: implement skeleton on about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,11 +1,12 @@
-import { Box, Button, Divider, Heading, Link, Stack, Text, VStack } from '@chakra-ui/react';
+import { Box, Heading, Text, VStack, Skeleton } from "@chakra-ui/react";
 
-import Head from 'next/head'
-import IlusProfile from '@/components/ilus-profile'
+import Head from "next/head";
+import Image from "next/image";
 import { NextSeo } from "next-seo";
-import React from 'react'
+import React from "react";
 
 const About: React.FC = () => {
+  const [loaded, setLoaded] = React.useState(true);
   return (
     <>
       <Head>
@@ -15,38 +16,62 @@ const About: React.FC = () => {
 
       <NextSeo
         openGraph={{
-          title: 'About Me',
-          description: 'Collage student that enjoy creating digital product',
-          url: 'https://joshuanatanielm.space/about',
+          title: "About Me",
+          description: "Collage student that enjoy creating digital product",
+          url: "https://joshuanatanielm.space/about",
           images: [
             {
-              url: 'https://joshuanatanielm.space/favicon.png',
+              url: "https://joshuanatanielm.space/favicon.png",
               width: 850,
               height: 650,
-              alt: 'Profile Photo',
+              alt: "Profile Photo",
             },
           ],
         }}
       />
 
-      <Box fontFamily='poppins' bgGradient="linear(to-br, white, blue.100)" borderRadius="xl" p={{ base: 8, md: 24 }} spacing={6}>
-        <VStack textAlign='center' py='50px' spacing='24px'>
-          <IlusProfile />
-          <Heading fontFamily='poppins' as='h1' size='2xl'>
+      <Box
+        fontFamily="poppins"
+        bgGradient="linear(to-br, white, blue.100)"
+        borderRadius="xl"
+        p={{ base: 8, md: 24 }}
+        spacing={6}
+      >
+        <VStack textAlign="center" py="50px" spacing="24px">
+          <Box>
+            <Image
+              src="/profileilus.png"
+              alt="profile"
+              width="200"
+              height="200"
+              onLoad={() => setLoaded(false)}
+            />
+            {loaded && <Skeleton w="full" width="200" height="200" />}
+          </Box>
+          <Heading fontFamily="poppins" as="h1" size="2xl">
             Hello i'am Jojo. 👋
           </Heading>
-          <Box fontSize={{ base: 'xl', md: '2xl' }} textAlign='left'>
-            <Text >
-              I’am a self taught Website developer based in <span style={{ fontWeight: 'bold' }}>Surabaya, Indonesia</span>, currently i working on my bachelor's degree in information system on <span style={{ fontWeight: 'bold' }}>Institut Teknologi Adhi Tama Surabaya</span> and doing some freelance projects.
+          <Box fontSize={{ base: "xl", md: "2xl" }} textAlign="left">
+            <Text>
+              I’am a self taught Website developer based in{" "}
+              <span style={{ fontWeight: "bold" }}>Surabaya, Indonesia</span>,
+              currently i working on my bachelor's degree in information system
+              on{" "}
+              <span style={{ fontWeight: "bold" }}>
+                Institut Teknologi Adhi Tama Surabaya
+              </span>{" "}
+              and doing some freelance projects.
             </Text>
             <Text>
-              i start learning about web dev since 2018, and start with backend development using php and laravel. but for now i spend more time to explore about frontend development espicially using Reactjs.
+              i start learning about web dev since 2018, and start with backend
+              development using php and laravel. but for now i spend more time
+              to explore about frontend development espicially using Reactjs.
             </Text>
           </Box>
         </VStack>
       </Box>
     </>
-  )
-}
+  );
+};
 
-export default About
+export default About;
