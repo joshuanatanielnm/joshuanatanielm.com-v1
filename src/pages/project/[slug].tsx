dca2c612cc2a05d579850cd401223d803c923452
Joshua Nathaniel M <clsjoshua@gmail.com>
2021-08-09 21:15:59 +0700

    feat: implement skeleton on project image

--- src/pages/project/[slug].tsx
+++ src/pages/project/[slug].tsx
@@ -1,20 +1,32 @@
-import { Box, HStack, Heading, Image, Img, Link, ListItem, Spacer, Stack, Text, UnorderedList, VStack } from '@chakra-ui/react';
+import {
+  Box,
+  HStack,
+  Heading,
+  Image,
+  Link,
+  ListItem,
+  Skeleton,
+  Stack,
+  Text,
+  UnorderedList,
+  VStack,
+} from "@chakra-ui/react";
 import type { GetStaticPaths, GetStaticProps, NextPage } from "next";
 
 import { FaExternalLinkAlt } from "react-icons/fa";
-import Head from 'next/head'
+import Head from "next/head";
 import { NextSeo } from "next-seo";
-import {Project} from '@/generated/graphql'
-import React from 'react'
+import { Project } from "@/generated/graphql";
+import React from "react";
 import { cms } from "@/lib/cms";
 
 interface ProjectPageProps {
-  project: Project
+  project: Project;
 }
 
-export const getStaticProps: GetStaticProps = async ({params}) => {
-  const slug = params.slug as string
-  const data = await cms().projectPageStaticProps({slug});
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const slug = params.slug as string;
+  const data = await cms().projectPageStaticProps({ slug });
 
   return {
     props: {
@@ -32,7 +44,8 @@ export const getStaticPaths: GetStaticPaths = async () => {
   };
 };
 
-const ProjectPage:NextPage<ProjectPageProps> = ({project}) => {
+const ProjectPage: NextPage<ProjectPageProps> = ({ project }) => {
+  const [loaded, setLoaded] = React.useState(true);
   return (
     <>
       <Head>
@@ -56,63 +69,69 @@ const ProjectPage:NextPage<ProjectPageProps> = ({project}) => {
         }}
       />
 
-      <Box fontFamily='poppins' bgGradient="linear(to-br, white, blue.100)"  borderRadius="xl">
+      <Box
+        fontFamily="poppins"
+        bgGradient="linear(to-br, white, blue.100)"
+        borderRadius="xl"
+      >
         <Stack p={8} spacing={6}>
-          <Heading fontFamily='poppins' as='h2' size='xl' textAlign='center'>
+          <Heading fontFamily="poppins" as="h2" size="xl" textAlign="center">
             {project.name}
           </Heading>
-          <VStack borderRadius='lg'>
-            <Image src={project.image.url} borderRadius='xl' width='full' />
+          <VStack borderRadius="lg">
+            <Image
+              src={project.image.url}
+              borderRadius="xl"
+              width="full"
+              onLoad={() => setLoaded(false)}
+            />
+            {loaded && <Skeleton w="full" height="400px" />}
           </VStack>
           <Stack>
-            <Heading fontFamily='poppins' as='h3' size='lg' >
+            <Heading fontFamily="poppins" as="h3" size="lg">
               Description
             </Heading>
-            <Text fontSize='lg'>
-              {project.longDescription}
-            </Text>
+            <Text fontSize="lg">{project.longDescription}</Text>
           </Stack>
-          <Stack direction={{base:'column', sm:'row'}} spacing='10'>
+          <Stack direction={{ base: "column", sm: "row" }} spacing="10">
             <Stack>
-              <Heading fontFamily='poppins' as='h3' size='lg' >
+              <Heading fontFamily="poppins" as="h3" size="lg">
                 Stack
               </Heading>
-              <Box fontSize='lg'>
+              <Box fontSize="lg">
                 <UnorderedList>
-                  {project.stack.map((v) => <ListItem key={v}>{v}</ListItem>)}
+                  {project.stack.map((v) => (
+                    <ListItem key={v}>{v}</ListItem>
+                  ))}
                 </UnorderedList>
               </Box>
             </Stack>
             <Stack>
-              <Heading fontFamily='poppins' as='h3' size='lg' >
+              <Heading fontFamily="poppins" as="h3" size="lg">
                 Links
               </Heading>
-              <Box fontSize='lg'>
+              <Box fontSize="lg">
                 <UnorderedList>
-                  {
-                    project.linkWebsite && (
-                      <ListItem>
-                        <HStack>
-                          <Link href={project.linkWebsite} isExternal>
-                            <Text as='i'>website</Text>
-                          </Link>
-                          <FaExternalLinkAlt/>
-                        </HStack>
-                      </ListItem>
-                    )
-                  }
-                  {
-                    project.linkRepo && (
-                      <ListItem>
-                        <HStack>
-                          <Link href={project.linkRepo} isExternal>
-                            <Text as='i'>Repository</Text>
-                          </Link>
-                          <FaExternalLinkAlt/>
-                        </HStack>
-                      </ListItem>
-                    )
-                  }
+                  {project.linkWebsite && (
+                    <ListItem>
+                      <HStack>
+                        <Link href={project.linkWebsite} isExternal>
+                          <Text as="i">website</Text>
+                        </Link>
+                        <FaExternalLinkAlt />
+                      </HStack>
+                    </ListItem>
+                  )}
+                  {project.linkRepo && (
+                    <ListItem>
+                      <HStack>
+                        <Link href={project.linkRepo} isExternal>
+                          <Text as="i">Repository</Text>
+                        </Link>
+                        <FaExternalLinkAlt />
+                      </HStack>
+                    </ListItem>
+                  )}
                 </UnorderedList>
               </Box>
             </Stack>
@@ -120,7 +139,7 @@ const ProjectPage:NextPage<ProjectPageProps> = ({project}) => {
         </Stack>
       </Box>
     </>
-  )
-}
+  );
+};
 
 export default ProjectPage;
