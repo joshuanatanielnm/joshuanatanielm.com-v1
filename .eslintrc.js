3db26436d884b182f1dc8af5ce847e60da6eb0de
Joshua Nathaniel M <clsjoshua@gmail.com>
2021-08-09 21:14:41 +0700

    feat: update eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  plugins: ["@typescript-eslint", "simple-import-sort"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
